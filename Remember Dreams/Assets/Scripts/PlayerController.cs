4efa3c093311c91d19fcfc4c74d127856dd32238
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-06-15 12:55:08 +0200

    player states

--- Remember Dreams/Assets/Scripts/PlayerController.cs
+++ Remember Dreams/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    struct PlayerInput
+    {
+        public float move_x;
+        public bool jump;
+    }
+
+
     public enum PlayerStates
     {
         IDLE = 1,
@@ -22,6 +30,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigid_body;
     private Animator anim;
     private SpriteRenderer sprite_renderer;
+    private PlayerInput player_input;
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,59 +45,78 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         anim.SetInteger("State", (int)player_state);
+
+        if (rigid_body.velocity.x > 0)
+        {
+            sprite_renderer.flipX = false;
+        }
+        else if (rigid_body.velocity.x < 0)
+        {
+            sprite_renderer.flipX = true;
+        }
     }
     private void FixedUpdate()
     {
+        GetInput();
+        ChangeState();
+        PerformActions();
 
+    }
 
+    private void PerformActions()
+    {
         switch (player_state)
         {
             case PlayerStates.IDLE:
+                break;
             case PlayerStates.WALK:
                 HoritzontalMovement();
+                break;
+            case PlayerStates.JUMPING:
                 Jump();
                 break;
             default:
                 break;
         }
+    }
 
-       
+    private void GetInput()
+    {
+        player_input.jump = Input.GetKeyDown(KeyCode.Space);
+        player_input.move_x = Input.GetAxis("Horizontal");
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigid_body.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
-            player_state = PlayerStates.JUMPING;
-        }
+        rigid_body.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
     }
 
     private void HoritzontalMovement()
     {
-        float h_axis = Input.GetAxis("Horizontal");
-        if (h_axis != 0)
-        {
-            Vector2 curVel = rigid_body.velocity;
-            curVel.x = h_axis * velocity;
-            rigid_body.velocity = curVel;
+        Vector2 curVel = rigid_body.velocity;
+        curVel.x = player_input.move_x * velocity;
+        rigid_body.velocity = curVel;
+    }
 
-            if (curVel.x > 0)
-            {
-                sprite_renderer.flipX = false;
-            }
 
-            if (curVel.x < 0)
-            {
-                sprite_renderer.flipX = true;
-            }
-            if (player_state != PlayerStates.JUMPING || player_state != PlayerStates.AIR)
-                player_state = PlayerStates.WALK;
-        }
-        else
+    private void ChangeState()
+    {
+        switch (player_state)
         {
-            if (player_state != PlayerStates.JUMPING || player_state != PlayerStates.AIR)
-                player_state = PlayerStates.IDLE;
+            case PlayerStates.IDLE:
+                if (player_input.jump)
+                    player_state = PlayerStates.JUMPING;
+                if (player_input.move_x != 0)
+                    player_state = PlayerStates.WALK;
+                break;
+            case PlayerStates.WALK:
+                if (player_input.jump)
+                    player_state = PlayerStates.JUMPING;
+                if (player_input.move_x == 0)
+                    player_state = PlayerStates.IDLE;
+                break;
+            default:
+                break;
         }
     }
 }
