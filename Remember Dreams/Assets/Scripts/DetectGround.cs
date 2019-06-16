d7024d65355ab5adfa4310e729f4e13601d5127a
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2019-06-16 13:29:26 +0200

    transition between air and idle

---
+++ Remember Dreams/Assets/Scripts/DetectGround.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectGround : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            GameObject.Find("Player").GetComponent<PlayerController>().player_state = PlayerController.PlayerStates.IDLE;
+        }
+    }
+}
